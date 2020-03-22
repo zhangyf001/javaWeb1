63c7a8fae95c5ff5a7d814a066b7f4f0088eaec9
15566264585 <1379532153@qq.om>
2020-03-22 13:35:17 +0800

    使用Feign

--- springcloudStudentConsumer80/src/main/java/com/zyf/StudentConsumerFeignApplication_80.java
+++ springcloudStudentConsumer80/src/main/java/com/zyf/StudentConsumerFeignApplication_80.java
@@ -5,13 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 //注：消费者项目不需要注入数据源，所以这里的exclude表示排除数据源注入，不排除会报错
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 @EnableEurekaClient
-public class StudentConsumerApplication_80 {
+@EnableFeignClients
+public class StudentConsumerFeignApplication_80 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudentConsumerApplication_80.class, args);
+		SpringApplication.run(StudentConsumerFeignApplication_80.class, args);
 	}
 }
--- springcloudStudentConsumer80/src/main/java/com/zyf/controller/StudentConsumerController.java
+++ springcloudStudentConsumer80/src/main/java/com/zyf/controller/StudentConsumerController.java
@@ -1,10 +1,10 @@
 package com.zyf.controller;
 
 import com.zyf.entity.Student;
+import com.zyf.service.StudentClientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
-import javax.annotation.Resource;
 import java.util.List;
 
 
@@ -17,11 +17,14 @@ import java.util.List;
 @RequestMapping("/student")
 public class StudentConsumerController {
 
-	@Resource
-	private RestTemplate restTemplate;
-	
+	/*@Resource
+	private RestTemplate restTemplate;*/
+
+	@Autowired
+	private StudentClientService studentClientService;
+
 	//private final static String PRE_HOST="http://localhost:1008";
-	private final static String PRE_HOST="http://SPRINGCLUD-STUDENT:1008";
+	private final static String PRE_HOST="http://SPRINGCLUD-STUDENT";
 
 	/**
      * 添加或者修改学生信息
@@ -30,7 +33,8 @@ public class StudentConsumerController {
      */
     @PostMapping(value="/save")
     public boolean save(Student student){
-    	return restTemplate.postForObject(PRE_HOST+"/student/save", student, Boolean.class);
+//    	return restTemplate.postForObject(PRE_HOST+"/student/save", student, Boolean.class);
+		return studentClientService.save(student);
     }
      
     /**
@@ -40,7 +44,8 @@ public class StudentConsumerController {
     @SuppressWarnings("unchecked")
 	@GetMapping(value="/list")
     public List<Student> list(){
-        return restTemplate.getForObject(PRE_HOST+"/student/list", List.class);
+//        return restTemplate.getForObject(PRE_HOST+"/student/list", List.class);
+		return studentClientService.list();
     }
      
     /**
@@ -49,7 +54,8 @@ public class StudentConsumerController {
      */
     @GetMapping(value="/get/{id}")
     public Student get(@PathVariable("id") Integer id){
-        return restTemplate.getForObject(PRE_HOST+"/student/get/"+id, Student.class);
+//        return restTemplate.getForObject(PRE_HOST+"/student/get/"+id, Student.class);
+		return studentClientService.get(id);
     }
      
     /**
@@ -58,7 +64,8 @@ public class StudentConsumerController {
      */
     @GetMapping(value="/delete/{id}")
     public boolean delete(@PathVariable("id") Integer id){
-        return restTemplate.getForObject(PRE_HOST+"/student/delete/"+id, Boolean.class);
+//        return restTemplate.getForObject(PRE_HOST+"/student/delete/"+id, Boolean.class);
+		return studentClientService.delete(id);
     }
 	
 }
